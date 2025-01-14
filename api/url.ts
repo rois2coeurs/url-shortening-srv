cd1ee6cab84b8cbcb4763bbd5aa5526da34148e2
rois2coeurs <rois2coeurs@gmail.com>
2025-01-14 20:05:40 +0100

    url.ts refactor

--- api/url.ts
+++ api/url.ts
@@ -1,23 +1,26 @@
 import {UrlModel} from "../models/UrlModel.ts";
 
+function get() {
+    return new Response(JSON.stringify(UrlModel.getAll()), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+}
+
+async function post(req: Request) {
+    const {url} = await req.json();
+    new UrlModel(url);
+    return new Response("URL added", {
+        headers: {
+            "Content-Type": "text/plain",
+        },
+    });
+}
+
 export default async function handler(req: Request) {
-    if (req.method === "GET") {
-        console.log(UrlModel.getAll());
-        return new Response(JSON.stringify(UrlModel.getAll()), {
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-    }
-    if (req.method === "POST") {
-        const { url } = await req.json();
-        new UrlModel(url);
-        return new Response("URL added", {
-            headers: {
-                "Content-Type": "text/plain",
-            },
-        });
-    }
+    if (req.method === "GET") return get();
+    if (req.method === "POST") return await post(req);
     throw new Error("Method not allowed");
 }
 
